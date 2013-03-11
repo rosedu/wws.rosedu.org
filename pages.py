98b84e94e0b919fc254cd7c10833fe907cc1ba5c
Alex Morega <alex@grep.ro>
2013-03-11 16:53:47 +0200

    page about a talk

--- pages.py
+++ pages.py
@@ -8,3 +8,8 @@ pages = flask.Blueprint('pages', __name__)
 @pages.route('/')
 def home():
     return flask.render_template('home.html', talks=content.talks)
+
+
+@pages.route('/<talk_id>.html')
+def talk(talk_id):
+    return flask.render_template('talk.html', talk=content.talks[talk_id])
